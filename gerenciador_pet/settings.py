64d42d9cb86e638e62e4e6a5b18d30e8a49f3a0a
Lucas <lucas.otuyama@gmail.com>
2024-08-07 19:05:47 -0300

    WIP: commit penultima aula

--- gerenciador_pet/settings.py
+++ gerenciador_pet/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-$9ah+4^wme#%@f!yudcf8-t$1*b8vvri11=y5x7+)d^qmkhsyr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
 
 # AUTHENTICATION
 AUTH_USER_MODEL = 'pet_shop.Usuario'
